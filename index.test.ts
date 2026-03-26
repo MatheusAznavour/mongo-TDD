7f06ae39273fdc69156636d55c0aa08c01ea1b8d
MatheusAznavour <matheus.lins896@gmail.com>
2026-03-26 15:13:07 -0300

    #1 added tests

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,22 @@
 import request from "supertest";
 import { app } from "./app";
 
-describe("GET /todo", ()=>{
-    it("get all todos", ()=>{
+describe("CRUD /todo", ()=>{
+    it("GET all todo", ()=>{
         request(app)
             .get("/todo")
+            .expect("Content-Type", /json/)
             .expect(200)
-    })
-});
-
-describe("GET /todo", ()=>{
-
+    });
+    it("GET unique todo", ()=>{});
+    it("POST create a todo", async ()=>{
+        await request(app)
+            .post("/todo")
+            .send(`title=JEST working&done=true&difficulty_level=3`)
+            .expect(200, {
+                title: "JEST working",
+                done: true,
+                difficulty_level: 3
+            })
+    });
 });
